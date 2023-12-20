b085e7a9db9e4bbb90f01175c1003a273bfa2ed2
Kyle1028 <93895301+Kyle1028@users.noreply.github.com>
2023-12-21 01:31:06 +0800

    Create 房屋.c

---
+++ practice/房屋.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int h = 4; 
+
+    for (int i = 1; i <= h; i++) {
+        for (int j = 1; j <= h - i; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    
+    for (int i = 1; i <= 2 * h - 1; i++) {
+        printf("*");
+    }
+    
+    printf("\n");
+    
+    for (int i = 1; i <= h; i++) {
+        printf("*");
+
+        for (int j = 1; j <2 * h - 2 ; j++) {
+            printf(" ");
+        }
+
+        printf("*\n");
+    }
+    
+    for (int i = 1; i <= 2 * h - 1; i++) {
+        printf("*");
+    }
+    
+    return 0;
+}
